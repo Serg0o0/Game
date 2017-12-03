03b313e7885e5d76fbafd2bb5874f7460bd6a4e0
Turtsevich Sergey <serg.t-34@yandex.ru>
2017-12-03 20:45:50 +0300

    add move player

--- Star Wars/Star Wars/Star Wars.cpp
+++ Star Wars/Star Wars/Star Wars.cpp
@@ -1,11 +1,18 @@
-// Star Wars.cpp: определяет точку входа для консольного приложения.
-//
+// Star Wars.cpp: главный файл проекта.
 
 #include "stdafx.h"
+#include "Form1.h"
 
+using namespace StarWars;
 
-int _tmain(int argc, _TCHAR* argv[])
+[STAThreadAttribute]
+int main(array<System::String ^> ^args)
 {
+	// Включение визуальных эффектов Windows XP до создания каких-либо элементов управления
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false); 
+
+	// Создание главного окна и его запуск
+	Application::Run(gcnew Form1());
 	return 0;
 }
-
